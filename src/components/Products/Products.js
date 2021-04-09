773c0d539781b78edcb98381d44a1de6d47fa1c4
tmhuyett <toddhuyett@gmail.com>
2021-04-09 18:23:30 -0400

    'd'

---
+++ src/components/Products/Products.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { graphql, StaticQuery } from 'gatsby'
+import ProductCard from './ProductCard'
+
+const containerStyles = {
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  justifyContent: 'space-between',
+  padding: '1rem 0 1rem 0',
+}
+
+const Products = () => {
+  return (
+<></>
+  )
+}
+
+export default Products
